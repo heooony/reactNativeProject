c7b6dfa86f11cffb8e42bede4ca5cd5f9d585fbd
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-11-28 00:03:15 +0900

    listView add

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,42 @@
-import React from "react"
-import {View, Text, TouchavleOpacity, StyleSheet} from "react-native"
+import React, { useState } from "react"
+import {View,
+        Text,
+        TouchavleOpacity,
+        StyleSheet,
+        Dimensions} from "react-native"
+
+const{ width, height } = Dimensions.get("window")
+
 export default function ToDo() {
+    const[editing, isEditing] = useState("")
     return(
-        <View>
-            <Text style={styles.text}>Hello</Text>
+        <View style={style.container}>
+            <TouchableOpacity>
+                <View style={styles.circle}/>
+            </TouchableOpacity>
+            <Text style={style.text}>Hello</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    circle: {
+      width: 50,
+      height: 50,
+      borderRadius: 20,
+      borderColor: "red",
+      borderWidth:5,
+      marginRight: 20
+    },
     text: {
-        fontSize: 50
+        fontWeight: "600",
+        fontSize: 20,
+        marginVertical: 20
     }
 })
